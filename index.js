0fe0658d18fe8b43f58e0d3cb318d8b8781b5c3f
zahira <essalouanyzahira14@gmail.com>
2024-11-28 12:55:40 +0100

    z

--- index.js
+++ index.js
@@ -42,16 +42,14 @@
 
 
 
+// JavaScript to swap images
+const smallImages = document.querySelectorAll('.small_img'); // Collect all small images
+const productImg = document.getElementById('ProductImg');  // Main product image
 
-
-
-
-/*=========== SELECT IMAGE ===========*/
-const ProductImg = document.getElementById("ProductImg")
-const smallImg = document.getElementsByClassName("small_img")
-for (let i=0;i<smallImg.length;i++){
-    smallImg[i].onclick = function(){
-        ProductImg.src = smallImg[i].src;
-    };
-}
-/*=========== FORM ===========*/
+// Add click event to each small image
+smallImages.forEach(image => {
+    image.addEventListener('click', function() {
+        // Change the main image source when a small image is clicked
+        productImg.src = this.src;
+    });
+});
